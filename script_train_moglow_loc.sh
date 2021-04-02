3b0064df05ba8d7ff809eabe8ea1af898585e5c5
Guillermo Valle <guillefix@gmail.com>
2021-04-02 20:46:24 +0000

    Moglow training script

--- script_train_moglow_loc.sh
+++ script_train_moglow_loc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TPU_IP_ADDRESS=10.8.195.90;
+export TPU_IP_ADDRESS=10.112.91.170;
 export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
 export TPU_NAME="grpc://$TPU_IP_ADDRESS:8470"
 #export XRT_TPU_CONFIG="tpu_worker;0;"
@@ -11,22 +11,22 @@ py=python
 dataset=multimodal
 model=transflower
 #exp=aistpp_big
-exp=aistpp_flower_mfix
+exp=moglow_loc
 
-$py training/train.py --data_dir=data/scaled_features --dataset_name=$dataset --model=$model --batch_size=4 --num_windows=1 --max_epochs=20000\
+$py training/train.py --data_dir=data/moglow_loc --dataset_name=$dataset --model=$model --batch_size=128 --num_windows=1 --max_epochs=20000\
     --experiment_name=$exp\
     --lr_policy="multistep" \
     --lr_decay_milestones="[5000,10000]" \
-    --learning_rate=6e-5 \
-    --dins="219,103" \
-    --douts="219" \
-    --input_modalities="joint_angles_scaled,mel_ddcpca_scaled" \
-    --output_modalities="joint_angles_scaled" \
-    --input_lengths="120,240" \
+    --learning_rate=2e-4 \
+    --dins="63,3" \
+    --douts="63" \
+    --input_modalities="moglow_loc,moglow_loc_control" \
+    --output_modalities="moglow_loc" \
+    --input_lengths="10,10" \
     --output_lengths="1" \
-    --output_time_offset="120" \
+    --output_time_offset="10" \
     --predicted_inputs="0,0" \
-    --nlayers=10 \
+    --nlayers=6 \
     --nhead=10 \
     --num_glow_coupling_blocks=5 \
     --glow_use_attn \
@@ -37,8 +37,8 @@ $py training/train.py --data_dir=data/scaled_features --dataset_name=$dataset --
     --dropout=0 \
     --workers=$(nproc) \
     --tpu_cores=8 \
+    --continue_train \
 #    --gradient_clip_val=0.5 \
-#    --continue_train \
 #    --accelerator=ddp \
 #    --gpus=0 \
 #    --log_every_n_steps=1 \
