1902bd099f04e8030cf0422332e34c5a4b2dc018
Guillermo Valle <guillefix@gmail.com>
2021-04-16 14:07:06 +0200

    fixed some things with generation code

--- data/copy_from_gs.sh
+++ data/copy_from_gs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 mkdir features
-gsutil -m cp gs://metagen/data/features_20/*expmap_scaled_20* features/
-gsutil -m cp gs://metagen/data/features_20/*mel_ddcpca_scaled_20* features/
-gsutil -m cp gs://metagen/data/features_20/*scaler* features/
-gsutil -m cp gs://metagen/data/features_20/*data_pipe* features/
-gsutil -m cp gs://metagen/data/features_20/*base_filenames* features/
+gsutil -m cp -r gs://metagen/data/moglow_pos ./
+gsutil -m cp -r gs://metagen/data/aistpp_20hz ./
+gsutil -m cp -r gs://metagen/data/aistpp_60hz ./
+#gsutil -m cp gs://metagen/data/features_20/*mel_ddcpca_scaled_20* features/
+#gsutil -m cp gs://metagen/data/features_20/*scaler* features/
+#gsutil -m cp gs://metagen/data/features_20/*data_pipe* features/
+#gsutil -m cp gs://metagen/data/features_20/*base_filenames* features/
 
-#if you don't have gsutil then use this
+#if you don't have gsutil then use this for aistpp_20hz
 #curl -L https://kth.box.com/shared/static/zd4b27jhrn819vkzlvmpkuhofaehslvo.gz --output features_20.tar.gz
 
+# but you can download gsutil here curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-336.0.0-linux-x86_64.tar.gz
+# and follow instructions here https://cloud.google.com/storage/docs/gsutil_install#linux 
