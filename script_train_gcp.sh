6c6891c62ac414ad5a2d001bf473d368d9f5a53d
Guillermo Valle <guillefix@gmail.com>
2021-04-21 23:32:38 +0000

    added gcp train script

---
+++ script_train_gcp.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+export TPU_IP_ADDRESS=10.32.188.58;
+export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+export TPU_NAME="grpc://$TPU_IP_ADDRESS:8470"
+#export XRT_WORKERS="localservice:0;grpc://localhost:40934"
+#export XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0|GPU:0;/job:localservice/replica:0/task:0/device:XLA_GPU:0"
+#export PYTHONPATH=$SCRATCH/:${PYTHONPATH}
+#export PYTHONPATH=/gpfsscratch/rech/imi/usc19dv/lib/python3.7/site-packages:${PYTHONPATH}
+
+py=python3
+
+#root_dir=$SCRATCH/data
+root_dir=data
+
+####aistpp_60hz
+#data_dir=${root_dir}/scaled_features
+#hparams_file=aistpp_60hz/transflower_aistpp_expmap
+#hparams_file=aistpp_60hz/transglower_aistpp_expmap
+
+####aistpp_20hz
+data_dir=${root_dir}/aistpp_20hz
+exp=$1
+#exp=transglower_aistpp_expmap
+#exp=transglower_residual_aistpp_expmap
+#exp=transflower_residual_aistpp_expmap
+#exp=transflower_aistpp_expmap
+#exp=residualflower2_transflower_aistpp_expmap
+#exp=moglow_aistpp_expmap
+hparams_file=aistpp_20hz/${exp}
+
+## Fix: needs vmapped version of transformer:
+#hparams_file=aistpp_20hz/residualflower2_moglow_aistpp_expmap
+
+#exp=${exp}_future3_actnorm
+#exp=${exp}_future3
+exp=${exp}_future3
+
+echo $exp
+
+$py training/train.py --data_dir=${data_dir} --max_epochs=1000\
+    --do_validation \
+    --hparams_file=training/hparams/${hparams_file}.yaml \
+    --val_batch_size=32 \
+    --batch_size=32 \
+    --experiment_name=$exp\
+    --workers=$(nproc) \
+    --output_lengths="3" \
+    --tpu_cores=8 \
+    #--accelerator=ddp \
+    #--scales="[[16,0]]" \
+#    --use_x_transformers \
+#    --use_rotary_pos_emb \
+    #--residual_scales="[[16,0]]"
+#    --glow_norm_layer="actnorm" \
+#    --continue_train \
+    #--use_pos_emb_output \
+    #--gpus=2 \
