88d94c4d9006ebe5173e3ecc25dc45c62948e109
Guillermo Valle <guillefix@gmail.com>
2021-05-04 14:04:28 +0000

    a

--- feature_extraction/motion_feature_extraction.sh
+++ feature_extraction/motion_feature_extraction.sh
@@ -22,7 +22,8 @@ param=expmap
 mpirun -n $n $py feature_extraction/process_motions.py $@ --param ${param} --fps $fps
 #mpirun -n 1 $py feature_extraction/extract_transform2.py $1 --feature_name bvh_${param} --transforms scaler
 mpirun -n $n $py feature_extraction/apply_transforms.py $@ --feature_name bvh_${param} --transform_name scaler --new_feature_name ${param}_scaled_${fps}
-cp $1/motion_expmap_data_pipe.sav $1/motion_${param}_scaled_${fps}_data_pipe.sav
+#cp $1/motion_expmap_data_pipe.sav $1/motion_${param}_scaled_${fps}_data_pipe.sav
+
 #cp $1/bvh_expmap_scaler.pkl $1/${param}_scaled_${fps}_scaler.pkl
 
 # for moglow
